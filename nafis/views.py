0c73692f20c7dcff7b38c20c84490c2bfafbc7f1
ashkanusefi <yousefi.ashkan96@gmail.com>
2021-05-17 18:11:31 +0330

    first commit

---
+++ nafis/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+
+
+def home(request):
+    context = {
+
+    }
+    return render(request, "nafis/home.html", context)
+
+
+def about(request):
+    context = {
+
+    }
+    return render(request, "nafis/about.html", context)
+
+
+def conection(request):
+    context = {
+
+    }
+    return render(request, "nafis/conection.html", context)
+
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+
+def kardarmani(request):
+    context = {
+
+    }
+    return render(request, "nafis/kardarmani.html", context)
+
+
+def goftar(request):
+    context = {
+
+    }
+    return render(request, "nafis/goftar.html", context)
+
+
+def fani(request):
+    context = {
+
+    }
+    return render(request, "nafis/fani.html", context)
+
+
+def pezeshki(request):
+    context = {
+
+    }
+    return render(request, "nafis/pezeshki.html", context)
+
+
+def parastari(request):
+    context = {
+
+    }
+    return render(request, "nafis/parastari.html", context)
+
+
+def fisoterpy(request):
+    context = {
+
+    }
+    return render(request, "nafis/fisoterpy.html", context)
